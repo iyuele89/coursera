425b3fbd62875e92d76c113e4f5e98bb5056e3bb
ARTI <arti1117.github.io>
2021-05-12 23:59:01 +0900

    Complete WEEK2

--- Machine Learning/Exercise/machine-learning-ex1/ex1/ex1_multi.m
+++ Machine Learning/Exercise/machine-learning-ex1/ex1/ex1_multi.m
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+new_house_xj = [ 1650, 3];
+[new_house_xj_normalized, mu_new_house, std_new_house] = featureNormalize(new_house_xj);
+price = [ 1, new_house_xj_normalized ] * theta;
 
 % ============================================================
 
@@ -149,7 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta; % You should change this
+
 
 
 % ============================================================
